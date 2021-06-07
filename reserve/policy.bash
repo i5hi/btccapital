43ed6078eb158b99698ef2d78bdb27bc8559967f
Vishal Menon <vishalmenon.92@gmail.com>
2021-06-07 19:48:44 +0530

    Added reserve policy implementation

---
+++ reserve/policy.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+HDPATH="m/84h/1h/0h"
+
+CTOMASTERPRV=$(bdk-cli key generate | jq -r ".xprv")
+CEOMASTERPRV=$(bdk-cli key generate | jq -r ".xprv")
+CFOMASTERPRV=$(bdk-cli key generate | jq -r ".xprv")
+
+CTOCHILDPRV=$(bdk-cli key derive --path $HDPATH --xprv $CTOMASTERPRV | jq -r ".xprv" | rev | cut -c3- | rev)
+CTOCHILDPUB=$(bdk-cli key derive --path $HDPATH --xprv $CTOMASTERPRV | jq -r ".xpub" | rev | cut -c3- | rev)
+
+CEOCHILDPRV=$(bdk-cli key derive --path $HDPATH --xprv $CEOMASTERPRV | jq -r ".xprv" | rev | cut -c3- | rev)
+CEOCHILDPUB=$(bdk-cli key derive --path $HDPATH --xprv $CEOMASTERPRV | jq -r ".xpub" | rev | cut -c3- | rev)
+
+CFOCHILDPRV=$(bdk-cli key derive --path $HDPATH --xprv $CFOMASTERPRV | jq -r ".xprv" | rev | cut -c3- | rev)
+CFOCHILDPUB=$(bdk-cli key derive --path $HDPATH --xprv $CFOMASTERPRV | jq -r ".xpub" | rev | cut -c3- | rev)
+
+
+POLICY="thresh(3,pk($CTOCHILDPUB),pk($CEOCHILDPUB),pk($CFOCHILDPUB))" 
+echo $POLICY
+
+DESC=$(bdk-cli compile "$POLICY" -t sh)
+
+echo $DESC
