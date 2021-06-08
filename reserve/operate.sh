110c2477b2609e049173b56d601afe20309cf518
Vishal Menon <vishalmenon.92@gmail.com>
2021-06-09 03:37:15 +0530

    Updated working operation

--- reserve/operate.sh
+++ reserve/operate.sh
@@ -1,12 +1,49 @@
 #!/bin/bash
 
-DESC="wsh(and_v(or_c(pk([d8aae923/84'/1'/1']tpubDDUXg7Fp9HCUrDtBm8HbejsBjMeun2Zgzm2zgULzHWTwLy4286jr7SedMwbc1P2Yep6oUKoqqc7Lk3zCbiXyViECNstnjPBUjmDUmgfzuUy),v:pk([a6404c30/84'/1'/3']tpubDDFHfHwJ1h82pYRjzSUdUZzjpTb8CqunVr8pkp5EfZGUTqdHgLFgJEL56VfXYbJfDEUQjqPPGgy7n5Z6eck918yW1WDAAQgtdtEgMWEZgsm)),and_v(or_c(pk([3d6b435e/84'/1'/1']tpubDCZVUAnqysBtWoNeYS2eAy2SYagy61fpENmmQTRJd1bCPCKua5DDboNeJgmi7H68hewN2exoRdUff9yqgVAMDd1nMBQ1ut8FG57KEr8tEPq),v:pk([77c87c6d/84'/1'/3']tpubDDHeuwFMZ95i2WaFfK1GCGY3PEB6VmnTiQwVetdN4omsuVzzY2vDZr2GCDSq3c9iXNxXPYN1Tjvf6gQpxwiC9wZRZyroA77Cr3VhRRN4dnp)),or_b(pk([89fa0709/84'/1'/1']tpubDDV66i76Y3MXYmnKPrqXHbHhr5uUWTPNjpREmdDdXkXMc8wpdfkuJzWpExW4icpawbKYxPP2a5mPA132xTAxrchXqRsaz96NG2g7vDvUjMf),s:pk([45b25bc5/84'/1'/3']tpubDDV4rGLwzrHqdoQNtyKiFEoqCyjXUUXUXEz634ePBELdzqCDBNcpXhZEJQZjRYAxpKff8jnw6Wrpo6zG8EhUkAJp2yjBFBFgEdFY5G8h1tQ)))))#m3a42exm"
-WALL=test67
+PUBDESC="wsh(and_v(or_c(pk([d8aae923/84'/1'/1']tpubDDUXg7Fp9HCUrDtBm8HbejsBjMeun2Zgzm2zgULzHWTwLy4286jr7SedMwbc1P2Yep6oUKoqqc7Lk3zCbiXyViECNstnjPBUjmDUmgfzuUy),v:pk([a6404c30/84'/1'/3']tpubDDFHfHwJ1h82pYRjzSUdUZzjpTb8CqunVr8pkp5EfZGUTqdHgLFgJEL56VfXYbJfDEUQjqPPGgy7n5Z6eck918yW1WDAAQgtdtEgMWEZgsm)),and_v(or_c(pk([3d6b435e/84'/1'/1']tpubDCZVUAnqysBtWoNeYS2eAy2SYagy61fpENmmQTRJd1bCPCKua5DDboNeJgmi7H68hewN2exoRdUff9yqgVAMDd1nMBQ1ut8FG57KEr8tEPq),v:pk([77c87c6d/84'/1'/3']tpubDDHeuwFMZ95i2WaFfK1GCGY3PEB6VmnTiQwVetdN4omsuVzzY2vDZr2GCDSq3c9iXNxXPYN1Tjvf6gQpxwiC9wZRZyroA77Cr3VhRRN4dnp)),or_b(pk([89fa0709/84'/1'/1']tpubDDV66i76Y3MXYmnKPrqXHbHhr5uUWTPNjpREmdDdXkXMc8wpdfkuJzWpExW4icpawbKYxPP2a5mPA132xTAxrchXqRsaz96NG2g7vDvUjMf),s:pk([45b25bc5/84'/1'/3']tpubDDV4rGLwzrHqdoQNtyKiFEoqCyjXUUXUXEz634ePBELdzqCDBNcpXhZEJQZjRYAxpKff8jnw6Wrpo6zG8EhUkAJp2yjBFBFgEdFY5G8h1tQ)))))"
+ADESC="wsh(and_v(or_c(pk([d8aae923/84'/1'/1']tpubDDUXg7Fp9HCUrDtBm8HbejsBjMeun2Zgzm2zgULzHWTwLy4286jr7SedMwbc1P2Yep6oUKoqqc7Lk3zCbiXyViECNstnjPBUjmDUmgfzuUy),v:pk([a6404c30/84'/1'/3']tpubDDFHfHwJ1h82pYRjzSUdUZzjpTb8CqunVr8pkp5EfZGUTqdHgLFgJEL56VfXYbJfDEUQjqPPGgy7n5Z6eck918yW1WDAAQgtdtEgMWEZgsm)),and_v(or_c(pk([3d6b435e/84'/1'/1']tpubDCZVUAnqysBtWoNeYS2eAy2SYagy61fpENmmQTRJd1bCPCKua5DDboNeJgmi7H68hewN2exoRdUff9yqgVAMDd1nMBQ1ut8FG57KEr8tEPq),v:pk([77c87c6d/84'/1'/3']tpubDDHeuwFMZ95i2WaFfK1GCGY3PEB6VmnTiQwVetdN4omsuVzzY2vDZr2GCDSq3c9iXNxXPYN1Tjvf6gQpxwiC9wZRZyroA77Cr3VhRRN4dnp)),or_b(pk([89fa0709/84'/1'/1']tprv8go3xJ4rPffrfJkXWDAvtBdbH4PYM8CUAWpTV7BL7Uixmeh41GwK8Vtx4q1X2W2ZaFJF3HfDvWpGMDK3czcZk4XJJBZ6TYCAmPCmnpWeY15),s:pk([45b25bc5/84'/1'/3']tpubDDV4rGLwzrHqdoQNtyKiFEoqCyjXUUXUXEz634ePBELdzqCDBNcpXhZEJQZjRYAxpKff8jnw6Wrpo6zG8EhUkAJp2yjBFBFgEdFY5G8h1tQ)))))"
+BDESC="wsh(and_v(or_c(pk([d8aae923/84'/1'/1']tpubDDUXg7Fp9HCUrDtBm8HbejsBjMeun2Zgzm2zgULzHWTwLy4286jr7SedMwbc1P2Yep6oUKoqqc7Lk3zCbiXyViECNstnjPBUjmDUmgfzuUy),v:pk([a6404c30/84'/1'/3']tpubDDFHfHwJ1h82pYRjzSUdUZzjpTb8CqunVr8pkp5EfZGUTqdHgLFgJEL56VfXYbJfDEUQjqPPGgy7n5Z6eck918yW1WDAAQgtdtEgMWEZgsm)),and_v(or_c(pk([3d6b435e/84'/1'/1']tpubDCZVUAnqysBtWoNeYS2eAy2SYagy61fpENmmQTRJd1bCPCKua5DDboNeJgmi7H68hewN2exoRdUff9yqgVAMDd1nMBQ1ut8FG57KEr8tEPq),v:pk([77c87c6d/84'/1'/3']tprv8gbcmXD7QmQ393YTmfLfnrsvpCfALSbZ97LiNNb4eXyV51kDue6dPMQQ24DnEdQfMrya5aa6kqFej5M1TWhWuSQAt8pibE7xucpR2YRFBj9)),or_b(pk([89fa0709/84'/1'/1']tpubDDV66i76Y3MXYmnKPrqXHbHhr5uUWTPNjpREmdDdXkXMc8wpdfkuJzWpExW4icpawbKYxPP2a5mPA132xTAxrchXqRsaz96NG2g7vDvUjMf),s:pk([45b25bc5/84'/1'/3']tpubDDV4rGLwzrHqdoQNtyKiFEoqCyjXUUXUXEz634ePBELdzqCDBNcpXhZEJQZjRYAxpKff8jnw6Wrpo6zG8EhUkAJp2yjBFBFgEdFY5G8h1tQ)))))"
+CDESC="wsh(and_v(or_c(pk([d8aae923/84'/1'/1']tprv8gnVXhDZzuWoxkrPsUd1FLD5AL8ychNnRTSDPxJgsEfYWUoFVhvFvx2mBnuvFKdUZ84YcEHcCDXw1Wgdf3uafqJmGY5i369aMhopJyV3LWS),v:pk([a6404c30/84'/1'/3']tpubDDFHfHwJ1h82pYRjzSUdUZzjpTb8CqunVr8pkp5EfZGUTqdHgLFgJEL56VfXYbJfDEUQjqPPGgy7n5Z6eck918yW1WDAAQgtdtEgMWEZgsm)),and_v(or_c(pk([3d6b435e/84'/1'/1']tpubDCZVUAnqysBtWoNeYS2eAy2SYagy61fpENmmQTRJd1bCPCKua5DDboNeJgmi7H68hewN2exoRdUff9yqgVAMDd1nMBQ1ut8FG57KEr8tEPq),v:pk([77c87c6d/84'/1'/3']tpubDDHeuwFMZ95i2WaFfK1GCGY3PEB6VmnTiQwVetdN4omsuVzzY2vDZr2GCDSq3c9iXNxXPYN1Tjvf6gQpxwiC9wZRZyroA77Cr3VhRRN4dnp)),or_b(pk([89fa0709/84'/1'/1']tpubDDV66i76Y3MXYmnKPrqXHbHhr5uUWTPNjpREmdDdXkXMc8wpdfkuJzWpExW4icpawbKYxPP2a5mPA132xTAxrchXqRsaz96NG2g7vDvUjMf),s:pk([45b25bc5/84'/1'/3']tpubDDV4rGLwzrHqdoQNtyKiFEoqCyjXUUXUXEz634ePBELdzqCDBNcpXhZEJQZjRYAxpKff8jnw6Wrpo6zG8EhUkAJp2yjBFBFgEdFY5G8h1tQ)))))"
+
+PUBWALL=tpub87
+AWALL=ta87
+BWALL=tb87
+CWALL=tc87
+
 RETURNADD=mkHS9ne12qx9pS9VojpwU5xtRd4T7X7ZUt
 
-bdk-cli wallet -w $WALL -d $DESC sync
-bdk-cli wallet -w $WALL -d $DESC get_new_address
-bdk-cli wallet -w $WALL -d $DESC get_balance
+bdk-cli wallet -w $PUBWALL -d $PUBDESC sync
+bdk-cli wallet -w $PUBWALL -d $PUBDESC get_new_address
+bdk-cli wallet -w $PUBWALL -d $PUBDESC get_balance
 
-PSBT=$(bdk-cli wallet -w $WALL -d $DESC create_tx -a --to $RETURNADD:0 | jq -r ".psbt")
+PSBT0=$(bdk-cli wallet -w $PUBWALL -d $PUBDESC create_tx -a --to $RETURNADD:0 | jq -r ".psbt")
+printf "$PSBT0"
 
+# Sign or(C,C')
+
+PSBTC=$(bdk-cli wallet -w $CWALL -d $CDESC sign --psbt $PSBT0 | jq -r ".psbt")
+# printf "$PSBTC"
+
+# Sign or(B,B')
+
+PSBTB=$(bdk-cli wallet -w $BWALL -d $BDESC sign --psbt $PSBT0 | jq -r ".psbt")
+# printf "$PSBTB"
+
+# Sign or(A,A')
+
+PSBTA=$(bdk-cli wallet -w $AWALL -d $ADESC sign --psbt $PSBT0 | jq -r ".psbt")
+# printf "$PSBTA"
+
+# Combine and(or(B,B'),or(A,A'))
+
+PSBTBA=$(bdk-cli wallet -w $PUBWALL -d $PUBDESC combine_psbt --psbt $PSBTB --psbt $PSBTA | jq -r ".psbt")
+
+# Combine and(or(C,C'),and(or(B,B'),or(A,A'))) 
+
+PSBTCBA=$(bdk-cli wallet -w $PUBWALL -d $PUBDESC combine_psbt --psbt $PSBTC --psbt $PSBTBA | jq -r ".psbt")
+
+printf "$PSBTCBA"
+
+# Finalize 
+
+bdk-cli wallet -w $PUBWALL -d $PUBDESC finalize_psbt --psbt $PSBTCBA
---
+++ reserve/test/policy.bash
@@ -0,0 +1,144 @@
+#!/bin/bash -e
+
+
+# CREATE SEED AND MASTER ACCOUNT
+CEOMASTERPRV=$(bdk-cli key generate | jq -r ".xprv")
+CFOMASTERPRV=$(bdk-cli key generate | jq -r ".xprv")
+CTOMASTERPRV=$(bdk-cli key generate | jq -r ".xprv")
+
+# DERIVE CHILD KEYS
+HDPATH="m/84h/1h/1h"
+
+CEOCHILDPRV=$(bdk-cli key derive --path $HDPATH --xprv $CEOMASTERPRV | jq -r ".xprv" | rev | cut -c3- | rev)
+CEOCHILDPUB=$(bdk-cli key derive --path $HDPATH --xprv $CEOMASTERPRV | jq -r ".xpub" | rev | cut -c3- | rev)
+
+CFOCHILDPRV=$(bdk-cli key derive --path $HDPATH --xprv $CFOMASTERPRV | jq -r ".xprv" | rev | cut -c3- | rev)
+CFOCHILDPUB=$(bdk-cli key derive --path $HDPATH --xprv $CFOMASTERPRV | jq -r ".xpub" | rev | cut -c3- | rev)
+
+CTOCHILDPRV=$(bdk-cli key derive --path $HDPATH --xprv $CTOMASTERPRV | jq -r ".xprv" | rev | cut -c3- | rev)
+CTOCHILDPUB=$(bdk-cli key derive --path $HDPATH --xprv $CTOMASTERPRV | jq -r ".xpub" | rev | cut -c3- | rev)
+
+# CREATE REGULAR 3/3 MULTI
+
+PUBPOLICY="thresh(3,pk($CEOCHILDPUB),pk($CFOCHILDPUB),pk($CTOCHILDPUB))"
+CEOPOLICY="thresh(3,pk($CEOCHILDPRV),pk($CFOCHILDPUB),pk($CTOCHILDPUB))"
+CFOPOLICY="thresh(3,pk($CEOCHILDPUB),pk($CFOCHILDPRV),pk($CTOCHILDPUB))"
+CTOPOLICY="thresh(3,pk($CEOCHILDPUB),pk($CFOCHILDPUB),pk($CTOCHILDPRV))"
+
+# CREATE DESCRIPTORS
+
+PUBDESC=$(bdk-cli compile "$PUBPOLICY" -t wsh)
+CEODESC=$(bdk-cli compile "$CEOPOLICY" -t wsh)
+CFODESC=$(bdk-cli compile "$CFOPOLICY" -t wsh)
+CTODESC=$(bdk-cli compile "$CTOPOLICY" -t wsh)
+
+# PRINT DESCRIPTORS TO DFILE
+DFILE="descriptors.md"
+touch $DFILE
+printf "# 3/3 MULTI\n\n" > $DFILE
+
+printf "## PUBLIC DESCRIPTOR\n" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+printf "$PUBDESC" | jq -r ".descriptor" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+
+printf "## CEO PRIVATE DESCRIPTOR\n" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+printf "$CEODESC" | jq -r ".descriptor" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+
+printf "## CFO PRIVATE DESCRIPTOR\n" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+printf "$CFODESC" | jq -r ".descriptor" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+
+printf "## CTO PRIVATE DESCRIPTOR\n" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+printf "$CTODESC" | jq -r ".descriptor" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+
+
+
+# CREATE RAFT
+# CREATE SEED AND RAFT ACCOUNT
+
+CEOMASTERPRV=$(bdk-cli key generate | jq -r ".xprv")
+CFOMASTERPRV=$(bdk-cli key generate | jq -r ".xprv")
+CTOMASTERPRV=$(bdk-cli key generate | jq -r ".xprv")
+
+# DERIVE BACKUP KEYS 
+HDPATH="m/84h/1h/3h"
+
+CEOCHILDPRV1=$(bdk-cli key derive --path $HDPATH --xprv $CEOMASTERPRV | jq -r ".xprv" | rev | cut -c3- | rev)
+CEOCHILDPUB1=$(bdk-cli key derive --path $HDPATH --xprv $CEOMASTERPRV | jq -r ".xpub" | rev | cut -c3- | rev)
+
+CFOCHILDPRV1=$(bdk-cli key derive --path $HDPATH --xprv $CFOMASTERPRV | jq -r ".xprv" | rev | cut -c3- | rev)
+CFOCHILDPUB1=$(bdk-cli key derive --path $HDPATH --xprv $CFOMASTERPRV | jq -r ".xpub" | rev | cut -c3- | rev)
+
+CTOCHILDPRV1=$(bdk-cli key derive --path $HDPATH --xprv $CTOMASTERPRV | jq -r ".xprv" | rev | cut -c3- | rev)
+CTOCHILDPUB1=$(bdk-cli key derive --path $HDPATH --xprv $CTOMASTERPRV | jq -r ".xpub" | rev | cut -c3- | rev)
+
+KFILE="keys.md"
+
+touch $KFILE
+printf "# KEYS \n\n" > $KFILE
+
+printf "## CEO \n" >> $KFILE
+printf "$CEOCHILDPRV\n"  >> $KFILE
+printf "$CEOCHILDPRV1\n"  >> $KFILE
+
+printf "## CFO\n" >> $KFILE
+printf "$CFOCHILDPRV\n"  >> $KFILE
+printf "$CFOCHILDPRV1\n"  >> $KFILE
+
+
+printf "## CTO\n" >> $KFILE
+printf "$CTOCHILDPRV\n"  >> $KFILE
+printf "$CTOCHILDPRV1\n"  >> $KFILE
+
+# CREATE POLICIES
+
+# EACH ONLY MAINTAINS ONE PRIVATE KEY IN THEIR POLICY
+
+# THE BACKUP KEYS ONLY EXIST FOR NOW AS WATCH-ONLY BACKUPS
+
+PUBPOLICY="thresh(3,or(pk($CEOCHILDPUB),pk($CEOCHILDPUB1)),or(pk($CFOCHILDPUB),pk($CFOCHILDPUB1)),or(pk($CTOCHILDPUB),pk($CTOCHILDPUB1)))"
+
+CEOPOLICY="thresh(3,or(pk($CEOCHILDPRV),pk($CEOCHILDPUB1)),or(pk($CFOCHILDPUB),pk($CFOCHILDPUB1)),or(pk($CTOCHILDPUB),pk($CTOCHILDPUB1)))"
+CFOPOLICY="thresh(3,or(pk($CEOCHILDPUB),pk($CEOCHILDPUB1)),or(pk($CFOCHILDPRV),pk($CFOCHILDPUB1)),or(pk($CTOCHILDPUB),pk($CTOCHILDPUB1)))"
+CTOPOLICY="thresh(3,or(pk($CEOCHILDPUB),pk($CEOCHILDPUB1)),or(pk($CFOCHILDPUB),pk($CFOCHILDPUB1)),or(pk($CTOCHILDPRV),pk($CTOCHILDPUB1)))"
+
+# CREATE DESCRIPTORS
+
+PUBDESC=$(bdk-cli compile "$PUBPOLICY" -t wsh)
+
+CEODESC=$(bdk-cli compile "$CEOPOLICY" -t wsh)
+CFODESC=$(bdk-cli compile "$CFOPOLICY" -t wsh)
+CTODESC=$(bdk-cli compile "$CTOPOLICY" -t wsh)
+
+# PRINT DESCRIPTORS TO DFILE
+printf "\n\n# 3/3 MULTI w/BACKUP\n\n" >> $DFILE
+
+printf "## PUBLIC DESCRIPTOR\n" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+printf "$PUBDESC" | jq -r ".descriptor" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+
+printf "## CEO PRIVATE DESCRIPTOR\n" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+printf "$CEODESC" | jq -r ".descriptor" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+
+printf "## CFO PRIVATE DESCRIPTOR\n" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+printf "$CFODESC" | jq -r ".descriptor" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+
+printf "## CTO PRIVATE DESCRIPTOR\n" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+printf "$CTODESC" | jq -r ".descriptor" >> $DFILE
+printf "\n\`\`\`\n" >> $DFILE
+
+
+
+exit 0
