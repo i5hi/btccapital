f16a4e17b93cbafbde5a676fd1932505ea87bc19
Vishal Menon <vishalmenon.92@gmail.com>
2021-06-09 10:40:23 +0530

    Updated multi.sh

--- reserve/multi.sh
+++ reserve/multi.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-MPUBWALL=mpub55
-AWALL=ma55
-BWALL=mb55
-CWALL=mc55
+MPUBWALL=mpub51
+AWALL=ma51
+BWALL=mb51
+CWALL=mc51
 
 
-PUBDESC="wsh(multi(3,[5c2d0df1/84'/1'/1']tpubDDeH77RCJ2k7buYymiqs77Fs7VFmXuxYUWYiTL4Lm4BqeRNrkfYEugi4vExQHjH4uFFzmyfb7KUAkmbSRwaaVdU7nL6BdQsjp6STaE99GGv,[ecf17a88/84'/1'/1']tpubDCcZfSBxYVMu3LWKhkaoFiwyP7iGGyMXEu72wjM1KGr7cBTxQS13zB6UCVvUNwkwx2K4Xv4YkezafnVLzCH5pashQwigTW4brV1SgDKhtLQ,[9e4ffa64/84'/1'/1']tpubDDkx8RgEFx41eFRzRUpWf4pSjf58px5Gb2CbUgkojcc9AWs5BHrCvG1mxCsErqF3q73D1CuCeVo73uQTQNFm46QdH7ueXn3VAujJAiiadc5))#y7r8jnz9"
-ADESC="wsh(multi(3,[5c2d0df1/84'/1'/1']tprv8gxExhNx9f4SiSXBt5BGhhbkYTjqNamduCwwAp23LnPSow868GiejC6Ck8BGxGpXFfMcPZoP5QqsF2XsSy8tmowJ7a8LYw3NcQn1wszCuEA,[ecf17a88/84'/1'/1']tpubDCcZfSBxYVMu3LWKhkaoFiwyP7iGGyMXEu72wjM1KGr7cBTxQS13zB6UCVvUNwkwx2K4Xv4YkezafnVLzCH5pashQwigTW4brV1SgDKhtLQ,[9e4ffa64/84'/1'/1']tpubDDkx8RgEFx41eFRzRUpWf4pSjf58px5Gb2CbUgkojcc9AWs5BHrCvG1mxCsErqF3q73D1CuCeVo73uQTQNFm46QdH7ueXn3VAujJAiiadc5))#t7un3l9x"
-BDESC="wsh(multi(3,[5c2d0df1/84'/1'/1']tpubDDeH77RCJ2k7buYymiqs77Fs7VFmXuxYUWYiTL4Lm4BqeRNrkfYEugi4vExQHjH4uFFzmyfb7KUAkmbSRwaaVdU7nL6BdQsjp6STaE99GGv,[ecf17a88/84'/1'/1']tprv8fvXX29iQ7gE9sUXp6vCrKHrp6CL7eAcfbWFfDJhu13imhDBn3BTogUc2LWKXDTw6GHhwcYWZR5Qa3G7jkGuHmMd9PXeXoRbpdqX2MZrM5Q,[9e4ffa64/84'/1'/1']tpubDDkx8RgEFx41eFRzRUpWf4pSjf58px5Gb2CbUgkojcc9AWs5BHrCvG1mxCsErqF3q73D1CuCeVo73uQTQNFm46QdH7ueXn3VAujJAiiadc5))#55krtcwf"
-CDESC="wsh(multi(3,[5c2d0df1/84'/1'/1']tpubDDeH77RCJ2k7buYymiqs77Fs7VFmXuxYUWYiTL4Lm4BqeRNrkfYEugi4vExQHjH4uFFzmyfb7KUAkmbSRwaaVdU7nL6BdQsjp6STaE99GGv,[ecf17a88/84'/1'/1']tpubDCcZfSBxYVMu3LWKhkaoFiwyP7iGGyMXEu72wjM1KGr7cBTxQS13zB6UCVvUNwkwx2K4Xv4YkezafnVLzCH5pashQwigTW4brV1SgDKhtLQ,[9e4ffa64/84'/1'/1']tprv8h4uz1dz7aNLknQCXq9vFfALAdZCfctN1ibpCAiWKLokL2cJYu2cjmPun65jBHfPKHP2Hwqn3N46eQaPeKYcRsqbbHGEWDwwcSnpHgpfkxT))#wt4gkv8s"
+PUBDESC="wsh(multi(3,[b4e9f331/84'/1'/1']tpubDC69ZpQmVXAcSbYBXSi1dGPU9jqw1K6qfTQKAQAVggVLL5xCgz6J2oTVMa2JCXRz8rNTfMS14JzEa5oDguTWudfxKJCn1s8Pqim5LBGxZ7d,[1d13b52b/84'/1'/1']tpubDDG4omDmgjUMhxpF878xeudtFCuEwyyrnBmTXD9TrkDzPwXR6njXGSBPBs7Ri5hbyELTe4M8ZRsXs2VAUcV5wp9yXAJnsnJnRzchWvr3xyL,[9b10a92b/84'/1'/1']tpubDCWJhQCUK3qvp3bxvgHY4RiTJ7YZqQWXApfbBc55ypzS9CmuJWNFWjeTq5sYhnt13se7h4TL41PYc1Jt5DgmmUtFqU7CEZwtM31dADTD5JL))"
+ADESC="wsh(multi(3,[b4e9f331/84'/1'/1']tprv8fQ7RQNXM9UwZ8WPdo3RDrjMaiKzqyuw69oXst8CGQgwVbhS4bGhrJqdBQgn61ipq5WFjxT5SqqsQUeGNqD7XvZxQ6rm4BL3Yzz5QySnfWm,[1d13b52b/84'/1'/1']tpubDDG4omDmgjUMhxpF878xeudtFCuEwyyrnBmTXD9TrkDzPwXR6njXGSBPBs7Ri5hbyELTe4M8ZRsXs2VAUcV5wp9yXAJnsnJnRzchWvr3xyL,[9b10a92b/84'/1'/1']tpubDCWJhQCUK3qvp3bxvgHY4RiTJ7YZqQWXApfbBc55ypzS9CmuJWNFWjeTq5sYhnt13se7h4TL41PYc1Jt5DgmmUtFqU7CEZwtM31dADTD5JL))"
+BDESC="wsh(multi(3,[b4e9f331/84'/1'/1']tpubDC69ZpQmVXAcSbYBXSi1dGPU9jqw1K6qfTQKAQAVggVLL5xCgz6J2oTVMa2JCXRz8rNTfMS14JzEa5oDguTWudfxKJCn1s8Pqim5LBGxZ7d,[1d13b52b/84'/1'/1']tprv8ga2fMBXYMngpVnTETUNFVymgBPJnenxCtAgEh7ASURbZTGeUPuw5wZX1kHczUacUqnvcHxeyn82MRhhwEQn5xGH2biyxYJzq5M6EP4X5Ne,[9b10a92b/84'/1'/1']tpubDCWJhQCUK3qvp3bxvgHY4RiTJ7YZqQWXApfbBc55ypzS9CmuJWNFWjeTq5sYhnt13se7h4TL41PYc1Jt5DgmmUtFqU7CEZwtM31dADTD5JL))"
+CDESC="wsh(multi(3,[b4e9f331/84'/1'/1']tpubDC69ZpQmVXAcSbYBXSi1dGPU9jqw1K6qfTQKAQAVggVLL5xCgz6J2oTVMa2JCXRz8rNTfMS14JzEa5oDguTWudfxKJCn1s8Pqim5LBGxZ7d,[1d13b52b/84'/1'/1']tpubDDG4omDmgjUMhxpF878xeudtFCuEwyyrnBmTXD9TrkDzPwXR6njXGSBPBs7Ri5hbyELTe4M8ZRsXs2VAUcV5wp9yXAJnsnJnRzchWvr3xyL,[9b10a92b/84'/1'/1']tprv8fpGYzAEAgAFvaaB32cwf24Lj62dg5KcbX4ou62nZZC3JiX8g7YfLF2beySXThHkvesgzjVrvS3F3x3ahsXGyuPUKCudCxdFCCATi3n68jR))"
 
 RETURNADD=mkHS9ne12qx9pS9VojpwU5xtRd4T7X7ZUt
 
@@ -37,12 +37,7 @@ PSBTC=$(bdk-cli wallet -w $CWALL -d $CDESC sign --psbt $PSBTB | jq -r ".psbt")
 
 # printf "$PSBTC"
 
-# Combine all & Finalize 
-
-# PSBTCOM=$(bdk-cli wallet -w $MPUBWALL -d $PUBDESC combine_psbt --psbt $PSBTA --psbt $PSBTB --psbt $PSBTC | jq -r ".psbt")
-# PSBTFIN=$(bdk-cli wallet -w $MPUBWALL -d $PUBDESC finalize_psbt --psbt $PSBTC | jq -r ".psbt")
 
 # Broadcast 
-# bdk-cli wallet -w $CWALL -d $CDESC broadcast --psbt $PSBTC
+bdk-cli wallet -w $CWALL -d $CDESC broadcast --psbt $PSBTC
 
-bdk-cli wallet -w $MPUBWALL -d $PUBDESC broadcast --psbt $PSBTC
